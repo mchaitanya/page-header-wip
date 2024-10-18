208573bd0c8f80e8a10085dcfc68162a2be3e051
Chaitanya Malireddy <malireddyc@vmware.com>
2024-10-18 13:15:29 +0530

    Adds providers for animations, NG_EVENT_PLUGINS, & I18NCommonService

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { NG_EVENT_PLUGINS } from '@tinkoff/ng-event-plugins';
 
+import { I18NCommonService } from '@dpa/ui-common/services';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideAnimations(),
+    provideRouter(routes),
+    NG_EVENT_PLUGINS,
+    I18NCommonService,
+  ]
 };
